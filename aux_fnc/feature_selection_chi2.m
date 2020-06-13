0b2665f9499f3f685b2894d499c79c60694a0ea0
Diogo Freitas <dntfreitas123@gmail.com>
2020-06-13 03:58:37 +0100

    LMA + PSO with mRMR

--- aux_fnc/feature_selection_chi2.m
+++ aux_fnc/feature_selection_chi2.m
@@ -1,8 +1,21 @@
-function [input] = feature_selection_chi2(input,output)
+function [input] = feature_selection_chi2(input,output, name)
 %FEATURE_SELECTION_CHI2 Feature selection based on chi-square tests
 %   Univariate feature ranking for classification using chi-square tests
-[~,scores] = fscchi2(input', output');
+[idx,scores] = fscchi2(input', output');
 mask = scores > mean(scores);
 input = input(mask, :);
+
+figure
+yline(mean(scores), '-.r', 'LineWidth', 2)
+hold on
+bar(idx, scores)
+xlabel('Feature number')
+ylabel('Feature score')
+xlabel('Feature number');
+ylabel('Feature score');
+%legend('Feature','Threshold', 'Location','northwest');
+legend('Feature','Threshold');
+
+title(name);
 end
 
--- dataimportchi2.m
+++ dataimportchi2.m
@@ -6,26 +6,26 @@ for i=1:size(car,2)-1
 end
 input_car = mapstd(car_ord)';
 output_car = car(:, end)';
-input_car = feature_selection_chi2(input_car, output_car);
+input_car = feature_selection_chi2(input_car, output_car, 'Car Evaluation Dataset');
 output_car = toOneHotEncoder(car(:,end))';
 %% Parkinsons data set
 parkinsons = readmatrix('data/parkinsons');
 input_parkinsons  = [parkinsons(:, 1:16) parkinsons(:, 18:end)]';
 input_parkinsons = mapstd(input_parkinsons);
 output_parkinsons = parkinsons(:, 17)';
-input_parkinsons = feature_selection_chi2(input_parkinsons, output_parkinsons);
+input_parkinsons = feature_selection_chi2(input_parkinsons, output_parkinsons, 'Parkinsons Dataset');
 output_parkinsons = toOneHotEncoder(parkinsons(:, 17))';
 %% Red wine data set
 red_wine = readmatrix('data/red_wine');
 input_red_wine  = red_wine(:, 1:end-1)';
 input_red_wine = mapstd(input_red_wine);
 output_red_wine = red_wine(:, end)';
-input_red_wine = feature_selection_chi2(input_red_wine, output_red_wine);
+input_red_wine = feature_selection_chi2(input_red_wine, output_red_wine, 'Red Wine Dataset');
 output_red_wine = toOneHotEncoder(red_wine(:, end))';
 %% White wine data set
 white_wine = readmatrix('data/white_wine');
 input_white_wine  = white_wine(:, 1:end-1)';
 input_white_wine = mapstd(input_white_wine);
 output_white_wine = white_wine(:, end)';
-input_white_wine = feature_selection_chi2(input_white_wine, output_white_wine);
+input_white_wine = feature_selection_chi2(input_white_wine, output_white_wine, 'White Wine Dataset');
 output_white_wine = toOneHotEncoder(white_wine(:, end))';
